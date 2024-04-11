7421d42d1f2787430da709e18f61004ff6951c3a
envy <envy1396@gmail.com>
2024-04-11 23:17:00 +0300

    base implementation

--- Poikaparka_bot/Program.cs
+++ Poikaparka_bot/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace Paikaparka
+{
+	public static class Program
+	{
+		private static string ApiKey = "";
+		public static async Task Main(string[] args)
+		{
+			var botClient = new TelegramBotClient(ApiKey);
+
+			var cts = new CancellationTokenSource();
+
+			botClient.StartReceiving(
+				updateHandler: UpdateHandler,
+				pollingErrorHandler: PollingErrorHandler,
+				cancellationToken: cts.Token,
+				receiverOptions: new ReceiverOptions
+				{
+					AllowedUpdates = Array.Empty<UpdateType>()
+				});
+
+			Console.Read();
+			await cts.CancelAsync();
+		}
+
+		private static async Task PollingErrorHandler(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+		{
+			Console.WriteLine("Dead");
+		}
+
+		private static async Task UpdateHandler(ITelegramBotClient client, Update update, CancellationToken ct)
+		{
+			Console.WriteLine($"{update.Type} {update.Message.Text}");
+		}
+	}
+}
